df8c106c1585ee155445f678ee60867d4caad54d
Imran Hossain <imranhossainrana.2014@gmail.com>
2025-10-06 18:44:10 +0600

    add year in mentor

--- server/controllers/admin/mentorship-controller.js
+++ server/controllers/admin/mentorship-controller.js
@@ -9,6 +9,7 @@ const addMentorship = async (req, res) => {
       previous_title,
       email,
       gender,
+      year,
     } = req.body;
     const newMontorship = new Mentorship({
       name,
@@ -17,6 +18,7 @@ const addMentorship = async (req, res) => {
       previous_title,
       email,
       gender,
+      year,
     });
     await newMontorship.save();
     res
@@ -28,10 +30,42 @@ const addMentorship = async (req, res) => {
   }
 };
 
+// const getMentorships = async (req, res) => {
+//   try {
+//     const mentorships = await Mentorship.find({}).collation({ locale: 'en' }).sort({ name: 1 });
+//     res.status(200).json({ success: true, data: mentorships });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: "Internal server error" });
+//   }
+// };
+
 const getMentorships = async (req, res) => {
   try {
-    const mentorships = await Mentorship.find({}).collation({ locale: 'en' }).sort({ name: 1 });
-    res.status(200).json({ success: true, data: mentorships });
+    const mentorships = await Mentorship.find({})
+      .collation({ locale: "en" })
+      .sort({ name: 1 });
+
+    const groupedByYear = mentorships.reduce((acc, mentorship) => {
+      const year = mentorship.year || "Unknown Year";
+      if (!acc[year]) acc[year] = [];
+      acc[year].push(mentorship);
+      return acc;
+    }, {});
+
+    // Return as array (guaranteed order)
+    const sortedGroupedArray = Object.entries(groupedByYear)
+      .sort(([yearA], [yearB]) => {
+        const numA = isNaN(Number(yearA)) ? -Infinity : Number(yearA);
+        const numB = isNaN(Number(yearB)) ? -Infinity : Number(yearB);
+        return numB - numA; // descending
+      })
+      .map(([year, mentors]) => ({ year, mentors }));
+
+    res.status(200).json({
+      success: true,
+      data: sortedGroupedArray,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
@@ -48,6 +82,7 @@ const updateMentorship = async (req, res) => {
       previous_title,
       email,
       gender,
+      year,
     } = req.body;
     
     const findMentorship = await Mentorship.findById(id);
@@ -63,6 +98,7 @@ const updateMentorship = async (req, res) => {
       previous_title || findMentorship.previous_title;
       findMentorship.email = email ?? findMentorship.email;
     findMentorship.gender = gender || findMentorship.gender;
+    findMentorship.year = year || findMentorship.year;
 
     await findMentorship.save();
     res
